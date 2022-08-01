72e6121896da796301d984cd8d9dd53ee3cbe113
Robert Beckman <Mystik738@gmail.com>
2022-08-01 16:12:41 -0700

    comments, lightning, seeds

--- main.go
+++ main.go
@@ -56,9 +56,11 @@ func readTemplate(s string) ([][]string, int64, int64) {
 		if i%2 == 1 {
 			line = line[1:]
 		}
+		//Find the longest line for later
 		if int64(len(line)+3)/4 > xSize {
 			xSize = int64((len(line) + 3) / 4)
 		}
+		//Put our structure in the template
 		for x := 0; x < len(line)-1; x += 2 {
 			template[i] = append(template[i], line[x:x+2])
 		}
@@ -75,6 +77,13 @@ func initSave() SaveGame {
 	s := SaveGame{}
 	ID.ID = 1
 
+	//Need a way to input these values
+	s.Planet.PlanetIndex.Value = 2
+	s.Colony.Latitude.Value = 0
+	s.Colony.Longitude.Value = -128
+
+	s.Terrain.Seed.Value = s.Colony.Longitude.Value*1000 + s.Colony.Latitude.Value
+
 	//Hardcode some values
 	s.Version = 12
 	s.Resources.InmaterialResources.Amount.ResourceType.Value = "Coins"
@@ -85,7 +94,6 @@ func initSave() SaveGame {
 	//Maybe this could be randomized?
 	s.VisitorEvents.VisitorEvent.Type = "VisitorEventFugitives"
 	s.VisitorEvents.VisitorEvent.VisitorCount.Value = 6
-	s.Planet.PlanetIndex.Value = 3
 
 	//Randomize meteor seeds
 	meteors := make([]string, 30)
@@ -124,17 +132,21 @@ func initSave() SaveGame {
 }
 
 func addStructures(s *SaveGame, template [][]string, xSize int64, zSize int64) {
+	//TODO: This distance should be inputtable
 	vertDist := 20.0
+
+	//General calculations based on coordinate system
 	horzDist := math.Sin(math.Pi/3.0) * vertDist
 	offset := horzDist / 2.0
 	isOff := false
-
 	xMapOffset := (maxCoord+minCoord)/2 - ((float64(xSize) - 1.0) / 2.0 * horzDist)
 	zMapOffset := (maxCoord+minCoord)/2 - ((float64(zSize) - 1.0) / 2.0 * vertDist)
 
 	log.Debugf("Map offsets are %v, %v", xMapOffset, zMapOffset)
 	log.Infof("Making %v by %v structures", xSize, zSize)
 
+	//Some matrices to store values as needed
+	//TODO: Make these pointers to the Modules themselves
 	idGrid := make([][]int64, xSize)
 	posGrid := make([][]Position, xSize)
 	for i := range idGrid {
@@ -142,6 +154,9 @@ func addStructures(s *SaveGame, template [][]string, xSize int64, zSize int64) {
 		posGrid[i] = make([]Position, zSize)
 	}
 
+	//X and Z here are Modules only, not template coordinates
+	//Xt = Xm*2
+	//Zt = Zm*2 + 1
 	for x := 0; x < int(xSize); x++ {
 		xPos := float64(x)*horzDist + float64(xMapOffset)
 		for z := 0; z < int(zSize); z++ {
@@ -154,6 +169,7 @@ func addStructures(s *SaveGame, template [][]string, xSize int64, zSize int64) {
 				Z: zPos,
 			}
 
+			//Offset Y on planet 3 for water
 			if s.Planet.PlanetIndex.Value == 3 {
 				p1.Y = 4
 			}
@@ -165,13 +181,16 @@ func addStructures(s *SaveGame, template [][]string, xSize int64, zSize int64) {
 					s.Constructions.Construction = append(s.Constructions.Construction, c)
 					idGrid[x][z] = c.ID.Value
 					posGrid[x][z] = p1
-					//Connections
+
+					//Connections. Only connect with Modules that have been placed,
+					//For each module this should be max 3 others
 					if z > 0 && compareTemplate(template, x*2, z*2, "==") {
 						p := calcLinkPosition(p1, posGrid[x][z-1], template[x*2][z*2+1], template[x*2][z*2-1], vertDist)
 						ct := initConnection(p, 0, idGrid[x][z], idGrid[x][z-1])
 						s.Constructions.Construction = append(s.Constructions.Construction, ct)
 					}
 					if x > 0 {
+						//If we're offset, look at (x-1, z) and (x-1, z+1)
 						if isOff {
 							if compareTemplate(template, (x-1)*2+1, z*2+1, "\\\\") {
 								log.Debugf("Connecting %v and %v via %v", idGrid[x][z], idGrid[x-1][z], template[(x-1)*2+1][z*2+1])
@@ -185,7 +204,7 @@ func addStructures(s *SaveGame, template [][]string, xSize int64, zSize int64) {
 								ct := initConnection(p, 120, idGrid[x][z], idGrid[x-1][z+1])
 								s.Constructions.Construction = append(s.Constructions.Construction, ct)
 							}
-						} else {
+						} else { //If we're not offset, look at (x-1, z) and (x-1, z-1)
 							if compareTemplate(template, (x-1)*2+1, z*2, "\\\\") {
 								log.Debugf("Connecting %v and %v via %v", idGrid[x][z], idGrid[x-1][z-1], template[(x-1)*2+1][z*2])
 								p := calcLinkPosition(p1, posGrid[x-1][z-1], template[x*2][z*2+1], template[(x-1)*2][(z-1)*2+1], vertDist)
@@ -208,6 +227,7 @@ func addStructures(s *SaveGame, template [][]string, xSize int64, zSize int64) {
 
 }
 
+// Calculates the position of a connection between two Modules
 func calcLinkPosition(p1 Position, p2 Position, s1 string, s2 string, dist float64) Position {
 	//Calc percentage from p1
 	linkSize := dist - sizeToFloat[moduleSizes[s1]] - sizeToFloat[moduleSizes[s2]]
@@ -222,6 +242,7 @@ func calcLinkPosition(p1 Position, p2 Position, s1 string, s2 string, dist float
 	return p
 }
 
+//Initializes a Connection
 func initConnection(p Position, rotation float64, id1 int64, id2 int64) Construction {
 	c := initConstruction("Connection", p)
 	c.Orientation.Y = rotation
@@ -236,6 +257,7 @@ func initConnection(p Position, rotation float64, id1 int64, id2 int64) Construc
 	return c
 }
 
+//Initializes a Module
 func initModule(t string, p Position) Construction {
 	c := initConstruction("Module", p)
 	c.ModuleType.Value = moduleTypes[t]
@@ -250,6 +272,7 @@ func initModule(t string, p Position) Construction {
 	return c
 }
 
+//Initializes a construction
 func initConstruction(t string, p Position) Construction {
 	c := Construction{
 		Type:     t,
@@ -266,13 +289,15 @@ func initConstruction(t string, p Position) Construction {
 	return c
 }
 
+//Safely compares a template index to a value.
 func compareTemplate(template [][]string, x int, z int, val string) bool {
-	if len(template) >= x && len(template[x]) > z && template[x][z] == val {
+	if len(template) > x && len(template[x]) > z && template[x][z] == val {
 		return true
 	}
 	return false
 }
 
+//Adds people to the SaveGame.
 func addCharacters(s *SaveGame, workers int64, biologists int64, engineers int64, medics int64, guards int64) {
 	s.Characters.Character = make([]Character, 0)
 
@@ -293,6 +318,7 @@ func addCharacters(s *SaveGame, workers int64, biologists int64, engineers int64
 	}
 }
 
+//Initializes a character
 func initCharacter(s string) Character {
 	c := Character{
 		Type: "Colonist",
@@ -318,6 +344,7 @@ func initCharacter(s string) Character {
 	return c
 }
 
+//Adds Resources to the SaveGame
 func addResources(s *SaveGame) {
 	p := Position{
 		X: 1000,
@@ -332,6 +359,7 @@ func addResources(s *SaveGame) {
 	}
 }
 
+//Initializes a resource
 func initResource(s string, p Position) Resource {
 	r := Resource{
 		Type:     s,
@@ -344,6 +372,7 @@ func initResource(s string, p Position) Resource {
 	return r
 }
 
+//Write the save to file
 func writeSave(s *SaveGame) {
 	s.IDGenerator.NextID.Value = NextID(&ID)
 	s.IDGenerator.NextBotID.Value = NextID(&BotID)
@@ -354,6 +383,7 @@ func writeSave(s *SaveGame) {
 	checkErr(err)
 }
 
+//Reads the save from file and analyzes a few characteristics
 func readSave() {
 	var save SaveGame
 	fileContents, err := os.ReadFile("saves/save2.sav")
@@ -421,10 +451,11 @@ func readSave() {
 	log.Info(min)
 	log.Info(minID)
 	log.Info(maxID)
-	//log.Info("\"", strings.Join(moduleTypeSlice, "\",\n\""), "\"")
+	log.Info("\"", strings.Join(moduleTypeSlice, "\",\n\""), "\"")
 	log.Info(moduleTypeSize)
 }
 
+//Adds all techs to the SaveGame
 func addTechs(save *SaveGame) {
 	techs := []string{
 		"TechDrillerBot",
